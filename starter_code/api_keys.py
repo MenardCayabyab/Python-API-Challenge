463c31f18ce49f461a1e47f077325bab6f444dc2
MenardCayabyab <cayabyab.menard@ymail.com>
2023-06-08 18:53:52 -0700

    Added .gitignore file and vacation code

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "68c4c1cfa67395034c55cc080c1efa3f"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "prj_live_pk_c47f35f6c024b3f11bb03627dc83b34a23c1369b"
